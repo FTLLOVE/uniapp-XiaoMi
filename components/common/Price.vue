67275c439fc0fabc7e9277de64f1633cc486ba8e
woodensRangerWZJ <13029768866@163.com>
2019-10-02 00:52:00 +0800

    详情页基础详情和商品特性

--- components/common/Price.vue
+++ components/common/Price.vue
@@ -1,12 +1,30 @@
 <template>
-  <view class="mr-2 d-flex font-md c-main a-self-start">
-    <text class="font">￥</text>
+  <view 
+    class="mr-2 d-flex  c-main a-self-start"
+    :class="priceSize"
+   >
+    <text 
+      class="font"
+      :class="textSize"
+     >￥</text>
     <!-- 价格 -->
     <slot></slot>
   </view>
 </template>
 
 <script>
+  export default {
+    props:{
+      priceSize:{
+        type: String,
+        default: "font-md"
+      },
+      textSize:{
+        type:String,
+        default: "font-sm"
+      }
+    }
+  }
 </script>
 
 <style>
--- pages/detail/detail.vue
+++ pages/detail/detail.vue
@@ -1,32 +1,80 @@
 <template>
-	<view>
+  <view>
     <!-- 商品详情轮播图 -->
-		<index-swiper 
-      :swipers="banners"
-      height="750"
-      :preview="true"
-    ></index-swiper>
-	</view>
+    <index-swiper :swipers="banners" height="750" :preview="true"></index-swiper>
+    <!-- 基础详情 -->
+    <detail-info :detail="detail"></detail-info>
+    <!-- 商品特性 -->
+    <detail-attr :baseAttrs="baseAttrs"></detail-attr>
+  </view>
 </template>
 
 <script>
   import IndexSwiper from '@/components/index/IndexSwiper.vue'
-	export default {
-    components:{
-      IndexSwiper
+  import DetailInfo from '@/components/detail/DetailInfo.vue'
+  import DetailAttr from '@/components/detail/DetailAttr.vue'
+  export default {
+    components: {
+      IndexSwiper,
+      DetailInfo,
+      DetailAttr
     },
-		data() {
-			return {
-				banners:[
-          { src: 'https://i8.mifile.cn/v1/a1/6d13e060-d5c5-e610-88d0-80816fa2b0ce.webp'},
-          { src: 'https://i8.mifile.cn/v1/a1/aff0b715-e63a-68f3-91b0-46a22543555e.webp'}
-								],
-			}
-		},
-		methods: {
-			
-		}
-	}
+    data() {
+      return {
+        banners: [{
+            src: 'https://i8.mifile.cn/v1/a1/6d13e060-d5c5-e610-88d0-80816fa2b0ce.webp'
+          },
+          {
+            src: 'https://i8.mifile.cn/v1/a1/aff0b715-e63a-68f3-91b0-46a22543555e.webp'
+          }
+        ],
+        detail: {
+          title: "小米MIX3 6GB+128GB",
+          desc: "磁动力滑盖全面屏 / 前后旗舰AI双摄 / 四曲面彩色陶瓷机身 / 高效10W无线充电",
+          pprice: 3299
+        },
+        baseAttrs: [{
+            icon: "icon-cpu",
+            title: "CPU",
+            desc: "蛟龙845八核"
+          },
+          {
+            icon: "icon-cpu",
+            title: "CPU",
+            desc: "蛟龙845八核"
+          },
+          {
+            icon: "icon-cpu",
+            title: "CPU",
+            desc: "蛟龙845八核"
+          },
+          {
+            icon: "icon-cpu",
+            title: "CPU",
+            desc: "蛟龙845八核"
+          },
+          {
+            icon: "icon-cpu",
+            title: "CPU",
+            desc: "蛟龙845八核"
+          },
+          {
+            icon: "icon-cpu",
+            title: "CPU",
+            desc: "蛟龙845八核"
+          },
+          {
+            icon: "icon-cpu",
+            title: "CPU",
+            desc: "蛟龙845八核"
+          },
+        ]
+      }
+    },
+    methods: {
+
+    }
+  }
 </script>
 
 <style>
