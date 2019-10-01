40937c8a8397e7617031286a2e285b5ca8c27f9d
woodensRangerWZJ <13029768866@163.com>
2019-10-01 14:34:58 +0800

    搜索页变色标签

---
+++ components/search/RandomColor.vue
@@ -0,0 +1,35 @@
+<template>
+  <view
+    :style="RandomColor"
+    class="px-2 py-1 d-inline-block border m-1"
+  >
+    {{item.name}}
+  </view>
+</template>
+
+<script>
+  export default {
+    props:{
+      item: Object,
+      color: {
+        type: Boolean,
+        default: true
+      }
+    },
+    computed:{
+     RandomColor(){
+       let color = {
+         borderColor:[ '#EEA6AA','#DD6A4B','#98D5D8','#9DBE93','#BCCD99' ],
+         background: [ '#F8EAE9','#FFBEC6','#E8F6F6','#E4F5E2','#F2F6E8' ]
+       }
+       let idx = Math.floor(Math.random()*color.borderColor.length)
+       let bdc = color.borderColor[idx]
+       let bgc = color.background[idx]
+       return `background: ${bgc};border-color: ${bdc}`
+     }
+    }
+  }
+</script>
+
+<style>
+</style>
--- pages/search/search.vue
+++ pages/search/search.vue
@@ -1,16 +1,38 @@
 <template>
-	<view>
-		搜索页
+	<view>    
+		<card 
+      headTitle="热门搜索"
+      bodyImage="../../static/images/demo/search-banner.png"
+    ></card>
+    <!-- 多色按钮 -->
+    <view class="px-1 mb-2">
+        <random-color
+          v-for="(item,idx) in hot"
+          :key="idx"
+          :item="item"          
+        ></random-color>
+    </view>
 	</view>
 </template>
 
 <script>
+  import RandomColor from '@/components/search/RandomColor.vue'
 	export default {
+    components:{
+      RandomColor
+    },
 		data() {
 			return {
-				
+				hot:[
+              { name:'领券中心' },
+              { name:'Redmi K20' },
+              { name:'RedmiBook 14' },
+              { name:'智能积木 越野四驱车' },
+              { name:'手环 腕带' },
+            ]
 			}
 		},
+    
 		methods: {
 			
 		}
