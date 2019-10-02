cbc1029a8eb58780af32407b00bd12288a1c78c8
woodensRangerWZJ <13029768866@163.com>
2019-10-02 13:45:31 +0800

    详情页属性选择开发

--- pages/detail/detail.vue
+++ pages/detail/detail.vue
@@ -8,38 +8,38 @@
     <detail-attr :baseAttrs="baseAttrs"></detail-attr>
     <!-- 属性选择 -->
     <view class="p-2">
-    			<view class="rounded border bg-light-secondary">
-    				<uni-list-item>
-    					<view class="d-flex">
-    						<text class="mr-2 text-muted">已选</text>
-    						<text>火焰红 64G 标配</text>
-    					</view>
-    				</uni-list-item>
-    				<uni-list-item>
-    					<view class="d-flex">
-    						<text class="mr-2 text-muted">配送</text>
-    						<text class="mr-2">北京 东城区</text>
-    						<text class="c-main">现配</text>
-    					</view>
-    				</uni-list-item>
-    				<uni-list-item>
-    					<view class="d-flex a-center">
-    						<view class="text-muted font d-flex a-center mr-2">
-    							<view class="iconfont icon-finish c-main"></view>
-    							小米自营
-    						</view>
-    						<view class="text-muted font d-flex a-center mr-2">
-    							<view class="iconfont icon-finish c-main"></view>
-    							小米发货
-    						</view>
-    						<view class="text-muted font d-flex a-center mr-2">
-    							<view class="iconfont icon-finish c-main"></view>
-    							七天无理由退货
-    						</view>
-    					</view>
-    				</uni-list-item>
-    			</view>
-    		</view>
+      <view class="rounded border bg-light-secondary">
+        <uni-list-item>
+          <view class="d-flex">
+            <text class="mr-2 text-muted">已选</text>
+            <text>火焰红 64G 标配</text>
+          </view>
+        </uni-list-item>
+        <uni-list-item>
+          <view class="d-flex">
+            <text class="mr-2 text-muted">配送</text>
+            <text class="mr-2">北京 东城区</text>
+            <text class="c-main">现配</text>
+          </view>
+        </uni-list-item>
+        <uni-list-item>
+          <view class="d-flex a-center">
+            <view class="text-muted font d-flex a-center mr-2">
+              <view class="iconfont icon-finish c-main"></view>
+              小米自营
+            </view>
+            <view class="text-muted font d-flex a-center mr-2">
+              <view class="iconfont icon-finish c-main"></view>
+              小米发货
+            </view>
+            <view class="text-muted font d-flex a-center mr-2">
+              <view class="iconfont icon-finish c-main"></view>
+              七天无理由退货
+            </view>
+          </view>
+        </uni-list-item>
+      </view>
+    </view>
   </view>
 </template>
 
@@ -47,6 +47,7 @@
   import IndexSwiper from '@/components/index/IndexSwiper.vue'
   import DetailInfo from '@/components/detail/DetailInfo.vue'
   import DetailAttr from '@/components/detail/DetailAttr.vue'
+  import DetailComments from '@/components/detail/DetailComments.vue'
   import uniListItem from "@/components/uni-ui/uni-list-item/uni-list-item.vue"
 
   export default {
@@ -54,10 +55,49 @@
       IndexSwiper,
       DetailInfo,
       DetailAttr,
+      DetailComments,
       uniListItem
     },
     data() {
       return {
+        // 评论
+        comments: [{
+            userpic: "/static/images/demo/demo6.jpg",
+            username: "楚绵",
+            create_time: "2019-07-31",
+            goods_num: 123,
+            context: "评论内容",
+            imglist: [
+              "/static/images/demo/demo6.jpg",
+              "/static/images/demo/demo6.jpg",
+              "/static/images/demo/demo6.jpg"
+            ]
+          },
+          {
+            userpic: "/static/images/demo/demo6.jpg",
+            username: "楚绵",
+            create_time: "2019-07-31",
+            goods_num: 123,
+            context: "评论内容",
+            imglist: [
+              "/static/images/demo/demo6.jpg",
+              "/static/images/demo/demo6.jpg",
+              "/static/images/demo/demo6.jpg"
+            ]
+          },
+          {
+            userpic: "/static/images/demo/demo6.jpg",
+            username: "楚绵",
+            create_time: "2019-07-31",
+            goods_num: 123,
+            context: "评论内容",
+            imglist: [
+              "/static/images/demo/demo6.jpg",
+              "/static/images/demo/demo6.jpg",
+              "/static/images/demo/demo6.jpg"
+            ]
+          }
+        ],
         banners: [{
             src: 'https://i8.mifile.cn/v1/a1/6d13e060-d5c5-e610-88d0-80816fa2b0ce.webp'
           },
